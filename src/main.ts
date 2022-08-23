75f15015bb9023d0c4f6554758e9150260be5940
Ronald Bunk <info@azoraqua.com>
2022-08-23 02:51:31 +0200

    Implemented statistics.
    Signed-off-by: Ronald Bunk <info@azoraqua.com>

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { Octokit } from "@octokit/core";
-import { CategoryChannel, ChannelType, Client, PermissionsBitField } from "discord.js";
+import { Client } from "discord.js";
 import dotenv from 'dotenv';
 import { log } from "./logger.js";
+import initStatistics from "./modules/statistics.js";
 
 async function main() {
   dotenv.config();
@@ -28,64 +28,11 @@ async function main() {
     ]
   });
 
-  setInterval(async () => await updateStatistics(client), 1000);
+  initStatistics(client);
 
   client.login(process.env.APP_TOKEN).then(() => {
     log('INFO', `Logged in as ${client.user?.tag}`);
   });
-}
-
-type Statistics = { readonly member_count: number, readonly repository_count: number }
-
-async function updateStatistics(client: Client) {
-  const guild = client.guilds.cache.first()!!;
-
-  const statistics: Statistics = {
-    member_count: guild.memberCount!!,
-    repository_count: await fetchRepositoryCount()
-  };
-
-  for (const stat in statistics) {
-    const prefix = stat.split('_')
-      .map(p => `${p.charAt(0).toUpperCase() + p.toLowerCase().substring(1)}`)
-      .join(" ")
-      .concat(": ");
-
-    let channel = guild.channels.cache.find(ch => ch.name.startsWith(prefix));
-   
-    if (channel) {
-      await channel.delete();
-    }
-
-    if (!channel) {
-      channel = await guild.channels.create({
-         // @ts-ignore
-        name: `${prefix} ${statistics[stat]}`,
-        type: ChannelType.GuildCategory,
-        permissionOverwrites: [
-          { id: guild.id, allow: ["ViewChannel"], deny: ["SendMessages", "ManageChannels"] }
-        ]
-      });
-    }
-
-    // @ts-ignore
-    await channel.edit({ name: `${prefix} ${statistics[stat]}` });
-  }
-}
-
-async function fetchRepositoryCount(): Promise<number> {
-  const octokit = new Octokit({
-    auth: process.env.APP_GH_TOKEN
-  });
-
-  const repositories = await octokit.request('GET /orgs/{org}/repos', {
-    org: 'oneleif',
-    type: 'public',
-    per_page: 100,
-  });
-
-  // @ts-ignore
-  return repositories.data.length;
 }
 
 export default main();
---
+++ src/modules/statistics.ts
@@ -0,0 +1,54 @@
+import { Octokit } from "@octokit/core";
+import { ChannelType, Client } from "discord.js";
+
+export type Statistics = { readonly member_count: number, readonly repository_count: number }
+
+export default function init(client: Client) {
+    setInterval(async () => {
+        const guild = client.guilds.cache.first()!!;
+
+        const statistics: Statistics = {
+            member_count: guild.memberCount!!,
+            repository_count: await fetchRepositoryCount()
+        };
+
+        for (const stat in statistics) {
+            const prefix = stat.split('_')
+                .map(p => `${p.charAt(0).toUpperCase() + p.toLowerCase().substring(1)}`)
+                .join(" ")
+                .concat(": ");
+
+            let channel = guild.channels.cache.find(ch => ch.name.startsWith(prefix));
+
+            if (!channel) {
+                channel = await guild.channels.create({
+                    // @ts-ignore
+                    name: `${prefix} ${statistics[stat]}`,
+                    type: ChannelType.GuildVoice,
+                    position: 0,
+                    permissionOverwrites: guild.roles.cache.mapValues(r => {
+                        return { id: r.id, allow: ["ViewChannel"], deny: ["Connect",] }
+                    })
+                });
+            }
+
+            // @ts-ignore
+            await channel.edit({ name: `${prefix} ${statistics[stat]}` });
+        }
+    }, 1000);
+}
+
+async function fetchRepositoryCount(): Promise<number> {
+    const octokit = new Octokit({
+        auth: process.env.APP_GH_TOKEN
+    });
+
+    const repositories = await octokit.request('GET /orgs/{org}/repos', {
+        org: 'oneleif',
+        type: 'public',
+        per_page: 100,
+    });
+
+    // @ts-ignore
+    return repositories.data.length;
+}
